d15f28b23e91a84a16d702a42f865f7395c4115a
Simone Ingenito <simoneingenito@Mac.station>
2026-02-05 23:27:08 +0100

    Initialized spring service

---
+++ karate-flow-api/src/test/java/org/hinnamac/karate_flow_api/KarateFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hinnamac.karate_flow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarateFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
